094a9a4f0f4a0730406468686e8c5beac5fd8802
Rui Andrade <andrade.rui99@gmail.com>
2025-02-19 12:07:59 +0000

    List with dummy data

---
+++ src/views/List.vue
@@ -0,0 +1,34 @@
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Primeiro Nome</th>
+                <th>Último Nome</th>
+                <th>E-mail</th>
+            </tr>
+        </thead>
+        <tbody v-for="client in getClients()">
+            <tr>
+                <td>{{ client.id }}</td>
+                <td>{{ client.firstName }}</td>
+                <td>{{ client.lastName }}</td>
+                <td>{{ client.email }}</td>
+            </tr>
+        </tbody>
+        <div v-if="getClients().length === 0" style="color: red; font-weight: bold;">
+            Sem dados
+        </div>
+    </table>
+</template>
+
+<script>
+export default {
+    methods: {
+        getClients() {
+            return [{id: 1, firstName: "rui", lastName: "andrade", email: "test@test.com"}]
+        }
+    }
+}
+
+</script>
