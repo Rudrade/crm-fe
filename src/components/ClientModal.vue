883b914dd14912515e3807ded228ff8fde03af0d
Rui Andrade <andrade.rui99@gmail.com>
2025-02-25 11:53:16 +0000

    Feature/create client (#2)
    * modal criar/editar cliente
    
    * update-client

---
+++ src/components/ClientModal.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="r-modal" v-if="show" id="client-modal">
+        <div class="r-modal-content">
+            <div class="r-modal-header">
+                <h5>{{ this.client !== null ? "Editar" : "Criar" }} Cliente<span class="close" @click="closeModal()">&times;</span></h5>
+            </div>
+            <div class="mb-3">
+                <label for="inFirstName" class="form-label">Primeiro Nome</label>
+                <input type="text" class="form-control" id="inFirstName" v-model="client.firstName"/>
+            </div>
+            <div class="mb-3">
+                <label for="inLastName" class="form-label">Último Nome</label>
+                <input type="text" class="form-control" id="inLastName" v-model="client.lastName"/>
+            </div>
+            <div class="mb-3">
+                <label for="inEmail" class="form-label">E-Mail</label>
+                <input type="email" class="form-control" id="inEmail" v-model="client.email"/>
+            </div>
+            <button class="btn btn-secondary" @click="closeModal()">Fechar</button>
+            <button class="btn btn-primary" @click="postClient()">Gravar</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            client: {
+                id: 0,
+                firstName: "",
+                lastName: "",
+                email: ""
+            },
+            show: false
+        }
+    },
+    methods: {
+        showModal(client) {
+            if (client !== undefined) {
+                this.client = { ...client };
+            }
+            this.show = true;
+        },
+        closeModal() {
+            this.client = null;
+            this.show = false;
+        },
+        postClient() {
+            console.log("creating cliente");
+
+            const client = this.client;
+            console.log(client)
+            if (client === null) {
+                return;
+            }
+
+            axios.post("http://localhost:8080/crm/api/client", 
+                client
+            )
+            .then(() => {
+                this.closeModal();
+            });
+        }
+    }
+}
+</script>
--- src/views/List.vue
+++ src/views/List.vue
@@ -6,24 +6,32 @@
                 <th>Primeiro Nome</th>
                 <th>Último Nome</th>
                 <th>E-mail</th>
+                <th>Ações</th>
             </tr>
         </thead>
         <tbody class="table-group-divider">
-            <tr v-for="client in clients">
+            <tr v-for="client in clients" :key="client.id">
                 <td>{{ client.id }}</td>
                 <td>{{ client.firstName }}</td>
                 <td>{{ client.lastName }}</td>
                 <td>{{ client.email }}</td>
+                <td><button class="btn btn-secondary" @click="openModalClient(client)"><i class="bi bi-pencil-square"></i></button></td>
             </tr>
         </tbody>
         <div v-if="clients.length === 0" style="color: red; font-weight: bold;" class="">
             Sem dados
         </div>
     </table>
+
+    <button class="btn btn-secondary" @click="openModalClient(null)">Criar Cliente</button>
+    <button class="btn btn-primary" @click="getClients()" style="margin-left: 5px;"><i class="bi bi-arrow-clockwise"></i></button>
+
+    <ClientModal ref="clientModal"/>
 </template>
 
 <script>
-import axios from "axios"
+import axios from 'axios';
+import ClientModal from "@/components/ClientModal.vue";
 
 export default {
     data() {
@@ -33,6 +41,7 @@ export default {
     },
     methods: {
         async getClients() {
+            this.clients = [];
             const response = await axios.get("http://localhost:8080/crm/api/client");
             
             console.log(response);
@@ -40,10 +49,16 @@ export default {
             if (response.status === 200) {
                 response.data.map(client => this.clients.push(client));
             }
+        },
+        openModalClient(client) {
+            this.$refs.clientModal.showModal(client);
         }
     },
     beforeMount() {
         this.getClients();
+    },
+    components: {
+        ClientModal
     }
 }
 
